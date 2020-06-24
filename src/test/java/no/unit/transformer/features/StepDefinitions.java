8083bdbfbe0c04ebf2c7f51632e0626986f84e10
Arve Waltin <29583605+croonerg@users.noreply.github.com>
2020-06-24 22:31:12 +0200

    test: Cleaned up tests using Scenario Outline

--- src/test/java/no/unit/transformer/features/StepDefinitions.java
+++ src/test/java/no/unit/transformer/features/StepDefinitions.java
@@ -1,150 +1,87 @@
 package no.unit.transformer.features;
 
-import io.cucumber.java.PendingException;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import no.unit.transformer.Transformer;
 
-import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import picocli.CommandLine;
 
 public class StepDefinitions extends TestWiring {
 
-    public static final String SINGLE_OBJECT_JSON = "single_object.json";
+  private CommandLine cmd;
+  private StringBuilder command;
 
-    @Given("^the user has an application \"Transformer\" that has a command line interface$")
-    public void theUserHasAnApplicationThatHasACommandLineInterface() {
-        new Transformer();
-    }
+  @Before
+  public void setUp(Scenario scenario) {
+    System.out.println("------------------------------");
+    System.out.println("Starting - " + scenario.getName());
+    System.out.println("------------------------------");
 
-    @And("\"Transformer\" has a flag \"--input\" that takes a single argument that is a filename")
-    public void hasAFlagInputThatTakesASingleArgumentThatIsAFilename() {
-        throw new PendingException();
-    }
+    command = new StringBuilder("transform");
+  }
 
-    @And("\"Transformer\" has a flag \"--output\" that takes a single argument that is a filename")
-    public void hasAFlagOutputThatTakesASingleArgumentThatIsAFilename() {
-        throw new PendingException();
-    }
+  @After
+  public void tearDown(Scenario scenario) {
+    System.out.println("------------------------------");
+    System.out.println(scenario.getName() + " Status - " + scenario.getStatus());
+    System.out.println("------------------------------");
+  }
 
-    @And("\"Transformer\" has a flag \"--input-format\" that takes a single argument \"xml\" or \"json\"")
-    public void theTransformerHasAFlagInputFormatThatTakesASingleArgumentXmlOrJson() {
-        throw new PendingException();
-    }
+  // Background
+  @Given("^the user has an application \"Transformer\" that has a command line interface$")
+  public void theUserHasAnApplicationThatHasACommandLineInterface() {
+    cmd = new CommandLine(new Transformer());
+  }
 
-    @And("\"Transformer\" has a flag \"--output-format\" that takes a single argument \"xml\" or \"json\"")
-    public void theTransformerHasAFlagOutputFormatThatTakesASingleArgumentXmlOrJson() {
-        throw new PendingException();
-    }
+  @And("\"Transformer\" has a flag \"--input\" that takes a single argument that is a filename")
+  public void hasAFlagInputThatTakesASingleArgumentThatIsAFilename() {
+    command.append(" --input=%s");
+  }
 
-    @Given("the user has a file {string}")
-    public void theUserHasAFile(String filename) {
-        File actual = getFileFromResources(filename);
-        assertTrue(actual.exists());
-    }
+  @And("\"Transformer\" has a flag \"--output\" that takes a single argument that is a filename")
+  public void hasAFlagOutputThatTakesASingleArgumentThatIsAFilename() {
+    command.append(" --output=%s");
+  }
 
-    @Given("the user has an input file that contains an array that contains a single object")
-    public void theUserHasAnInputFileThatContainsAnArrayThatContainsASingleObject() {
-        File actual = getFileFromResources(SINGLE_OBJECT_JSON);
-        assertTrue(actual.exists());
-    }
+  @And(
+      "\"Transformer\" has a flag \"--output-format\" that takes a single argument \"xml\" or \"json\"")
+  public void theTransformerHasAFlagOutputFormatThatTakesASingleArgumentXmlOrJson() {
+    command.append(" --output-format=%s");
+  }
 
-    @And("the object has field {string} with string value {string}")
-    public void theObjectHasFieldWithStringValue(String field, String value) {
-        throw new PendingException();
-    }
+  @Given("the user has a file {string}")
+  public void the_user_has_a_file(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @When("the user transforms the data")
-    public void theUserTransformsTheData() {
-        throw new PendingException();
-    }
+  @When("the user transforms the file to {string}")
+  public void the_user_transforms_the_file_to(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @Then("the user sees that the output file contains an array that contains a single object")
-    public void theUserSeesThatTheOutputFileContainsAnArrayThatContainsASingleObject() {
-        throw new PendingException();
-    }
+  @Then("the user sees that the output file conforms to {string}")
+  public void the_user_sees_that_the_output_file_conforms_to(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @And("the object has a field {string} with an integer value {string}")
-    public void theObjectHasAFieldWithAnIntegerValue(String field, String integer) {
-        throw new PendingException();
-    }
+  @When("the user transforms the data")
+  public void the_user_transforms_the_data() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @And("the object has a field {string}")
-    public void theObjectHasAField(String field) {
-        throw new PendingException();
-    }
-
-    @And("the field \"identity\" contains an object with the fields \"given\" and \"family\"")
-    public void theFieldContainsAnObjectWithTheFieldsAnd() {
-        throw new PendingException();
-    }
-
-    @And("the field {string} contains string value {string}")
-    public void theFieldContainsStringValue(String field, String value) {
-        throw new PendingException();
-    }
-
-    @And("the data is formatted correctly")
-    public void theDataIsFormattedCorrectly() {
-        throw new PendingException();
-    }
-
-    @When("the user transforms the file from (.*) to (.*)")
-    public void theUserTransformsTheFileFromSerializationAToSerializationB(String serializationA,
-                                                                           String serializatiionB) {
-        throw new PendingException();
-    }
-
-    @And("they open the file")
-    public void theyOpenTheFile() {
-        throw new PendingException();
-    }
-
-    @Then("they see that the data is transformed to (.*)")
-    public void theySeeThatTheDataIsTransformedToSerializationB(String serialization) {
-        throw new PendingException();
-    }
-
-    @And("that the elements in \"users\" section of the file are ordered by element \"sequence\"")
-    public void thatTheElementsInSectionOfTheFileAreOrderedByElement() {
-        throw new PendingException();
-    }
-
-    @Given("the user has a (.*) in (.*)")
-    public void theUserHasAFileInSerialization(String filename, String serialization) {
-        throw new PendingException();
-    }
-
-    @When("they transform the file without specifying the output format flag")
-    public void theyTransformTheFileWithoutSpecifyingTheOutputFormatFlag() {
-        throw new PendingException();
-    }
-
-    @And("they open the transformed file")
-    public void theyOpenTheTransformedFile() {
-        throw new PendingException();
-    }
-
-    @Then("the file is transformed to serialization")
-    public void theFileIsTransformedToSerialization() {
-        throw new PendingException();
-    }
-
-    @And("the data is formatted badly")
-    public void theDataIsFormattedBadly() {
-        throw new PendingException();
-    }
-
-    @When("the user attempts to transform the file")
-    public void theUserAttemptsToTransformTheFile() {
-        throw new PendingException();
-    }
-
-    @Then("they see an error message telling them that the input file is badly formatted")
-    public void theySeeAnErrorMessageTellingThemThatTheInputFileIsBadlyFormatted() {
-        throw new PendingException();
-    }
+  @Then("the user sees an error message telling that the input file is badly formatted")
+  public void the_user_sees_an_error_message_telling_that_the_input_file_is_badly_formatted() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
 }
